781e4a036c0de737f0baf593e697aeeed8518a3a
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-10-29 10:55:51 +0300

    solve first six challenges

---
+++ python/two-fer/two_fer.py
@@ -0,0 +1,9 @@
+"""Two-fer game"""
+
+def two_fer(name: str = "you") -> str:
+    """
+    :param name: str - just a name
+    :return: str - a sentence
+    """
+
+    return f"One for {name}, one for me."
